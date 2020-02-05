ea041597b588cf8dbcb1726f340ec3609440a89a
RIck Fung <rick2032@hotmail.com>
2020-02-05 00:08:17 -0800

    Setup to display list of test products
    Add to testing project, a mock library.  Then in main project, start the Domain Model along with the respository interface.  Later add the needed controller and view files to have a working displayable list.

--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -6,12 +6,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
+
 namespace SportsStore
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -20,6 +23,9 @@ namespace SportsStore
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvc(routes => {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Product}/{action=List}/{id?}");
             });
         }
     }

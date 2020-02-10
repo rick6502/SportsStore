9571a6891157e0f2ce49ee6fecab5ca375523f66
RIck Fung <rick2032@hotmail.com>
2020-02-09 18:48:18 -0800

    Made the app's URLs more user readable
    Modded the routing configuration to make the app's URLs into composable URLs ie user readable.

--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -35,8 +35,12 @@ namespace SportsStore
             app.UseStaticFiles();
             app.UseMvc(routes => {
                 routes.MapRoute(
-                name: "default",
-                template: "{controller=Product}/{action=List}/{id?}");
+                        name: "pagination",
+                        template: "Products/Page{productPage}",
+                        defaults: new { Controller = "Product", action = "List" });
+                routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Product}/{action=List}/{id?}");
             });
             SeedData.EnsurePopulated(app);
         }
